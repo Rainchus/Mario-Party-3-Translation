cdcc0998c58c9e136dad32330da834759053f60c
Rainchus <Rainchus@github.com>
2022-08-31 17:39:09 -0500

    change mv to move in build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 mkdir obj
 mips64-elf-gcc -Wall -O2 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c src/strings.c
-mv strings.o obj/
+move strings.o obj/
 armips asm/main.asm
 n64crc "rom/mp3mod.z64"
